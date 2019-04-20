d879167167030c325d334a31fbd914b2e8af1bd5
f*** you <beep@beep.com>
2019-04-20 15:55:31 -0700

    Backward String

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,8 +8,19 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
+		String n = "";
+		for(int i = s.length() - 1; i >= 0 ; i --) {
+			n = n + s.substring(i , i + 1);
+			
+//			System.out.println(i + ", "+ (i + 1) + ", " + (s.length() - i) + ", " + (s.length() - i - 1)) ;
+//			System.out.println(s.substring(i, i+1) + ", " + s.substring(s.length() - i - 1,s.length() - i ));
+//			String swap = s.substring(i, i+1);
+//			s = s.replace(s.substring(i, i+1), s.substring(s.length() - i - 1,s.length() - i )  );
+//			s = s.replace(s.substring(s.length() - i - 1,s.length() - i ) , swap);
+		}
 		
-		return "";
+		return n;
 	}
 
 }
+
